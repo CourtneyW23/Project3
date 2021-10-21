76ab482401012a2038d6658d1b26a0fc95e5b2b1
Courtney Williams <williams@opsys.cs.umsl.edu>
2021-10-20 23:30:00 -0500

    final

--- config.h
+++ config.h
@@ -4,4 +4,27 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+/* ARGSUSED */
+ static void myhandler(int s) {
+    char aster = '*';
+    int errsave;
+    errsave = errno;
+    write(STDERR_FILENO, &aster, 1);
+    errno = errsave;
+ }
+
+ static int setupinterrupt(void) {  /* set up myhandler for SIGPROF */
+    struct sigaction act;
+    act.sa_handler = myhandler;
+    act.sa_flags = 0;
+    return (sigemptyset(&act.sa_mask) || sigaction(SIGPROF, &act, NULL));
+ }
+
+ static int setupitimer(void) {    /* set ITIMER_PROF for 2-second intervals */
+    struct itimerval value;
+    value.it_interval.tv_sec = 2;
+    value.it_interval.tv_usec = 0;
+    value.it_value = value.it_interval;
+    return (setitimer(ITIMER_PROF, &value, NULL));
+ }
 
--- license.h
+++ license.h
@@ -1,14 +1,9 @@
 
-typedef struct license{
-	time_t time;
-	char *shm;
-} license;
-
-	int getlicense(void);
-	int returnlicense(void);
-	int initlicense(void);
-	int addtolicenses(int n);
-	int removelicenses(int n);
-	int logmsg(const char * msg);
+int getlicense(void);
+int returnlicense(void);
+int initlicense(void);
+int addtolicenses(int n);
+int removelicenses(int n);
+int logmsg(const char * msg);
 
 
